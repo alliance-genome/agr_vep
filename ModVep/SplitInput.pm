2cbe74b46f2be0646296c9b4a8c4240365fdb0ab
Mark Quinton-Tulloch <mqt@noah-login-01.ebi.ac.uk>
2020-11-03 11:32:42 +0000

    Fix issue with adding VCF headers

--- ModVep/SplitInput.pm
+++ ModVep/SplitInput.pm
@@ -20,7 +20,6 @@ sub fetch_input {
 
     my $split_file_length = 500000;
 
-    my $header_lines = $self->get_header_lines;
     my $vcf_file = $self->required_param('vcf');
     my $mod = $self->required_param('mod');
 
@@ -37,38 +36,42 @@ sub fetch_input {
 	my $file = $output_dir .'/' . $self->required_param('mod') . '_' . $n;
 	push @split_files, $file;
 	    
-	$self->add_header_lines($file, $header_lines, $output_dir)
+	$self->add_header_lines($file) if $n == 1;
 	    
     }
     
     $self->param('vep_output_ids', [map {{vep_input_file => $_}} @split_files]);
 }
 
-sub add_header_lines {
-    my ($self, $file, $header_lines, $output_dir) = @_;
 
-    $header_lines =~ s/"/\\"/g;
-    my $prepend_cmd = "sed -i '1s|^|${header_lines}|' $file";
-    my ($exit_code, $stderr, $flat_cmd) = $self->run_system_command($prepend_cmd);
+sub add_header_lines {
+    my ($self, $file) = @_;
+
+    my $tmp_file = $file . '.tmp';
+
+    my $vcf_file = $self->required_param('vcf');
+    open (TMP, '>', $tmp_file);
+    
+    open (HEADER, "grep '^#' $vcf_file |");
+    while (<HEADER>) {
+	print TMP $_;
+    }
+    close (HEADER);
+    
+    open (VEP, '<', $file);
+    while (<VEP>) {
+	print TMP $_;
+    }
+    close (VEP);
+
+    close (TMP);
+
+    my ($exit_code, $stderr, $flat_cmd) = $self->run_system_command("mv $tmp_file $file");
     die "Prepending headers to $file failed ($exit_code): $flat_cmd - $stderr" unless $exit_code == 0;
 
     return;
 }	    
 
-sub get_header_lines {
-    my $self = shift;
-
-    my $header_lines;
-    my $file = $self->required_param('vcf');
-    open (VCF, "grep '^#' $file |");
-    while (<VCF>) {
-	chomp;
-	$header_lines .= $_ . '\\n';
-    }
-    close VCF;
-
-    return $header_lines;
-}    
 
 sub write_output {
     my $self = shift;
