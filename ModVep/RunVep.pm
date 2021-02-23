42936a399fd6695d1ddcfdf33abda7b5c2a15f0d
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2021-02-23 23:55:11 +0000

    Run TranscriptName VEP plugin in MOD HTP VEP pipeline

--- ModVep/RunVep.pm
+++ ModVep/RunVep.pm
@@ -22,7 +22,7 @@ sub run {
 
     # Run VEP
     $self->dbc->disconnect_when_inactive(1);
-    my $cmd = "vep -i $input_file -gff $gff_file --format vcf -fasta $fasta_file --vcf --hgvs --hgvsg -shift_hgvs=0 --symbol --numbers --distance 0 --output_file $output_file --force_overwrite --plugin $plugin_str --plugin GenomePos";
+    my $cmd = "vep -i $input_file -gff $gff_file --format vcf -fasta $fasta_file --vcf --hgvs --hgvsg -shift_hgvs=0 --symbol --numbers --distance 0 --output_file $output_file --force_overwrite --plugin $plugin_str --plugin GenomePos --plugin TranscriptName,gff=$gff_file";
     
     if ($self->param('bam')) {
 	$cmd .= ' --bam ' . $self->param('bam');
