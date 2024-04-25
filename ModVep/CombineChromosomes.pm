26b5b08ecbffd4d208cc55147a9c68a1396d9244
wormbase <wormbase@codon-login-02.ebi.ac.uk>
2024-04-25 19:13:37 +0100

    Update file combination process

--- ModVep/CombineChromosomes.pm
+++ ModVep/CombineChromosomes.pm
@@ -10,34 +10,38 @@ sub run {
     my $self = shift;
 
     my $mod = $self->required_param('mod');
-    my $out_file = $self->required_param('out_file_prefix') . 'all.vcf';
+    my $out_file = $self->required_param('out_file_prefix') . 'vcf';
     my $out_fh = file($out_file)->openw;
-    if ($mod eq 'RGD' || $mod eq 'MGI' || $mod eq 'HUMAN') {
-	my $first_file = 1;
-	my $working_dir = dir($self->required_param('vep_working'));
-	for my $chromosome_dir ($working_dir->children()) {
-	    next unless $chromosome_dir->is_dir;
-	    for my $subdir ($chromosome_dir->children()) {
-		for my $file ($subdir->children()) {
-		    next unless $file->stringify() =~ /\.vep\.vcf\.processed$/;
-		    my $in_fh = file($file)->openr;
-		    while ( my $line = $in_fh->getline()) {
-			next if !$first_file and $line =~ /^#/;
-			chomp $line;
-			$out_fh->print($line . "\n");
-		    }
-		    $first_file = 0;
+    my $first_file = 1;
+    my $working_dir = dir($self->required_param('vep_working'));
+    for my $chromosome_dir ($working_dir->children()) {
+	next unless $chromosome_dir->is_dir;
+	for my $subdir ($chromosome_dir->children()) {
+	    for my $file ($subdir->children()) {
+		next unless $file->stringify() =~ /\.vep\.vcf\.processed$/;
+		my $in_fh = file($file)->openr;
+		while ( my $line = $in_fh->getline()) {
+		    next if !$first_file and $line =~ /^#/;
+		    chomp $line;
+		    $out_fh->print($line . "\n");
 		}
+		$first_file = 0;
 	    }
-	    my $err;
-	    remove_tree($chromosome_dir->stringify, {error => \$err});
-	    die "remove_tree failed for $chromosome_dir: " .Dumper($err) if $err && @$err;
 	}
+	my $err;
+	remove_tree($chromosome_dir->stringify, {error => \$err});
+	die "remove_tree failed for $chromosome_dir: " .Dumper($err) if $err && @$err;
+    }
 
-	for my $cmd( "bgzip -l -9 $out_file", "tabix -p vcf ${out_file}.gz") {
-	    my ($exit_code, $std_err, $flat_cmd) = $self->run_system_command($cmd);
-	    die "$cmd failed [$exit_code]: $std_err" unless $exit_code == 0;
-	}
+    if (-e $out_file . ".gz") {
+	system("rm ${out_file}.gz");
+    }
+    if (-e $out_file . ".gz.tbi") {
+	system("rm ${out_file}.gz.tbi");
+    }
+    for my $cmd( "bgzip -l 9 $out_file", "tabix -p vcf ${out_file}.gz") {
+	my ($exit_code, $std_err, $flat_cmd) = $self->run_system_command($cmd);
+	die "$cmd failed [$exit_code]: $std_err" unless $exit_code == 0;
     }
 }
 
